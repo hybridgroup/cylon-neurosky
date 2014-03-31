72411f05ed04c99233e86c92e227091709034c7e
deadprogram <ron@hybridgroup.com>
2014-03-31 09:35:01 -0700

    WIP on Mindwave

---
+++ examples/neuro.js
@@ -0,0 +1,15 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection:
+    {name: 'neurosky', adaptor: 'neurosky', port: '/dev/rfcomm0'},
+
+  device:
+    {name: 'headset', driver: 'neurosky'},
+
+  work: function(my) {
+    my.headset.on('data', function(data) {
+      Logger.info("data: " + data);
+    });
+  }
+}).start();
