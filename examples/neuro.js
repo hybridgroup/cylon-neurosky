ad6cfb7e21339fe4bbeac54be35ee43f723be227
deadprogram <ron@hybridgroup.com>
2014-03-31 10:57:21 -0700

    More WIP on reading Mindwave data

--- examples/neuro.js
+++ examples/neuro.js
@@ -8,6 +8,9 @@ Cylon.robot({
     {name: 'headset', driver: 'neurosky'},
 
   work: function(my) {
+    every((1).seconds(), function() {
+      Logger.info("running");
+    });
     my.headset.on('data', function(data) {
       Logger.info("data: " + data);
     });
