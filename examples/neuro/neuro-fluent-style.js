bdf18c8ec1cb663d369cec2b1a3e8d591a032793
edgarsilva <edgar.osc@gmail.com>
2014-10-15 14:08:11 -0500

    Added fluent style examples

---
+++ examples/neuro/neuro-fluent-style.js
@@ -0,0 +1,14 @@
+var cylon = require('cylon');
+
+cylon.robot({
+  connection: { name: 'neurosky', adaptor: 'neurosky', port: '/dev/rfcomm0' },
+  device: { name: 'headset', driver: 'neurosky' }
+})
+
+.on('ready', function(robot) {
+  robot.headset.on('packet', function(packet) {
+    console.log('packet:', packet);
+  });
+})
+
+.start();
